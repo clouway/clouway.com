6967dd748a069765f735c999b2b2a29bb25d8f00
Miroslav Genov <mgenov@gmail.com>
2017-04-02 15:20:34 +0300

    Publishing to gh-pages (publish.sh)

--- js/google_map.js
+++ js/google_map.js
@@ -33,12 +33,7 @@ function init() {
     var addresses = ['clouWay ltd, bulevard "Nikola Gabrovski", Veliko Tarnovo'];
 
     for (var x = 0; x < addresses.length; x++) {
-        var proto = 'http';
-        if (location.protocol == 'https:') {
-          proto = "https"
-        }
- 
-        $.getJSON(proto + '://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
+        $.getJSON('//maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
             var p = data.results[0].geometry.location
             var latlng = new google.maps.LatLng(p.lat, p.lng);
             new google.maps.Marker({
